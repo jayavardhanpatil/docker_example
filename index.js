b6b8136bec8f36d889f9f5754b092eaf27e4eaa8
jayavardhanpatil <jayavardhan.patil@gmail.com>
2019-09-18 01:11:32 -0700

    Changes to test Travis CI

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I love docker!");
+    res.json("Updated to test travis CI.");
 });
 
 app.listen(PORT, () => {
